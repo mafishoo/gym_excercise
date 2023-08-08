eb2c444f582f5aac6d0ec34265d4158c7c88e3f7
meraff123 <113929284+meraff123@users.noreply.github.com>
2023-08-07 20:02:20 -0500

    initial project layout

---
+++ src/components/excercises/Excercises.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Excercises.css"
+
+const Excercises = () => {
+  return (
+    <div>Excercises</div>
+  )
+}
+
+export default Excercises
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import { Box } from '@mui/material'
+import SearchExcercise from '../components/searchExcercise/SearchExcercise'
+import Excercises from '../components/excercises/Excercises'
+import HeroBanner from '../components/heroBanner/HeroBanner'
+
+
+const Home = () => {
+  return (
+    <Box> 
+    <HeroBanner/>
+    <SearchExcercise/>
+    <Excercises/>
+     </Box>
+  )
+}
+
+export default Home
